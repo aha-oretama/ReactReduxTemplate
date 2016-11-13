b6638aa2821ed5cf89340e3d0cb9eef9078149b4
aha-oremata <sekine_y_529@msn.com>
2016-11-13 14:54:44 +0900

    add Immutable.js

--- src/client/reducers/dog-reducer.js
+++ src/client/reducers/dog-reducer.js
@@ -4,14 +4,14 @@
  */
 import { MAKE_BARK } from '../actions/dog-actions';
 
-const initialState = {
+const initialState = Immutable.Map({
     hasBarked: false,
-};
+});
 
 const dogReducer = (state = initialState, action) => {
     switch (action.type) {
         case MAKE_BARK:
-            return { hasBarked: action.payload };
+            return state.set('hasBarked', action.payload);
         default:
             return state;
     }
